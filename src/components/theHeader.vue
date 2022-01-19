8bdc361e03712e5aa1190b83a6237c5228823948
Alexander Kozlov <kozloww1508@yandex.ru>
2022-01-19 22:20:21 +0300

    added new Vue 3 project, created templates of theHeader and theFooter components

---
+++ src/components/theHeader.vue
@@ -0,0 +1,68 @@
+<template>
+          <header class="header">
+         <nav class="header__menu-content">
+            <img class="header__close" src="img/close.svg" alt="close">
+            <p class="header__title">MENU</p>
+            <ul class="header__menu-list">
+               <li class="header__sub-title">MAN</li>
+               <li class="header__menu-item"> <a class="header__menu-link" href="#">Accessories</a></li>
+               <li class="header__menu-item"> <a class="header__menu-link" href="#">Poslání, cíle, fakta</a></li>
+               <li class="header__menu-item"> <a class="header__menu-link" href="#">Denim</a></li>
+               <li class="header__menu-item"> <a class="header__menu-link" href="#">T-Shirts</a></li>
+            </ul>
+            <p class="header__sub-title">WOMAN</p>
+            <ul class="header__menu-list">
+               <li class="header__menu-item"> <a class="header__menu-link" href="#">Accessories</a></li>
+               <li class="header__menu-item"> <a class="header__menu-link" href="#">Jackets & Coats</a></li>
+               <li class="header__menu-item"> <a class="header__menu-link" href="#">Polos</a></li>
+               <li class="header__menu-item"> <a class="header__menu-link" href="#">T-Shirts</a></li>
+               <li class="header__menu-item"> <a class="header__menu-link" href="#">Shirts</a></li>
+            </ul>
+            <p class="header__sub-title">KIDS</p>
+            <ul class="header__menu-list">
+               <li class="header__menu-item"> <a class="header__menu-link" href="#">Accessories</a></li>
+               <li class="header__menu-item"> <a class="header__menu-link" href="#">Jackets & Coats</a></li>
+               <li class="header__menu-item"> <a class="header__menu-link" href="#">Polos</a></li>
+               <li class="header__menu-item"> <a class="header__menu-link" href="#">T-Shirts</a></li>
+               <li class="header__menu-item"> <a class="header__menu-link" href="#">Shirts</a></li>
+               <li class="header__menu-item"> <a class="header__menu-link" href="#">Shirts</a></li>
+            </ul>
+         </nav>
+         <div class="header__inner">
+            <div class="container">
+               <div class="header__topline">
+                  <div class="header__topline-left">
+                     <a class="logo" href="index.html">
+                        <img class="header__logo" src="img/logo.svg" alt="logo">
+                     </a>
+                     <button class="header__search-btn">
+                        <img class="header__search-img" src="img/search.svg" alt="search"></button>
+                     <form class="header__search" action="form">
+                        <input class="header__search-field" type="search" placeholder="Search....">
+                     </form>
+                  </div>
+                  <div class="header__topline-right">
+                     <button class="header__menu">
+                        <span class="header__menu-line"></span>
+                        <span class="header__menu-line"></span>
+                        <span class="header__menu-line"></span>
+                     </button>
+                     <a class="header__topline-right-link" href="registration.html">
+                        <img class="header__topline-img" src="img/social.svg" alt="social">
+                     </a>
+                     <a class="header__topline-right-link" href="cart.html">
+                        <img src="img/cart.svg" alt="cart" class="cart">
+                        <div class="header__topline-right-count">0</div>
+                     </a>
+                  </div>
+               </div>
+
+            </div>
+         </div>
+      </header>
+</template>
+
+<script></script>
+<style scoped>
+
+</style>
