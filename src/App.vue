b2083d4d791ee0f6ff6d4c96470060271a1f6277
AnnaVishnyakova <simonya6ka@gmail.com>
2022-01-23 18:05:28 +0300

    catalogs+catalogItem в компоненте + App изменения

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <the-header />
+    <the-catalog />
+    <the-footer />
+    
+  </div>
+</template>
+
+<script>
+import theHeader from './components/theHeader.vue'
+import theFooter from './components/theFooter.vue'
+import theCatalog from './components/theCatalog.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    theHeader,
+    theFooter,
+    theCatalog
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/theCatalogItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <article class="products__item">
+        <div class="products__img">
+            <img class="products__img-card" src="img/fe-card-1.jpg" alt="product">
+            <div class="products__overlay">
+                <button class="products__btn">
+                    <img class="products__icon-cart" src="img/icon-cart.svg" alt="icon-cart">Add to Cart
+                </button>
+            </div>
+        </div>
+        <div class="products__content-title">
+            <h3 class="products__title">ELLERY X M'O CAPSULE</h3>
+            <p class="products__descr">Known for her sculptural takes on traditional tailoring,
+                Australian arbiter of
+                cool
+                Kym Ellery teams up
+                with Moda Operandi.</p>
+            <span class="products__price">$52.00</span>
+        </div>
+    </article>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
