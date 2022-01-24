f84bc215570803d666470e45eea6d09c592bd889
AnnaVishnyakova <simonya6ka@gmail.com>
2022-01-24 17:36:27 +0300

    second commit catalog

--- src/components/theCatalogItem.vue
+++ src/components/theCatalogItem.vue
@@ -12,7 +12,7 @@
             <h3 class="products__title">ELLERY X M'O CAPSULE</h3>
             <p class="products__descr">Known for her sculptural takes on traditional tailoring,
                 Australian arbiter of
-                cool
+                cooldsgsdgsgsg
                 Kym Ellery teams up
                 with Moda Operandi.</p>
             <span class="products__price">$52.00</span>
